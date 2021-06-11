fe8b4f03192b5d49686bc1c46c8c0836222380d4
Mohammed Mudassir Ali <alimudassir12@gmail.com>
2021-06-12 00:09:20 +0530

    Setup Contacts Context and Global State

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,64 @@
+import React, { useReducer } from 'react';
+import uuid from 'uuid';
+import ContactContext from './contactContext';
+import contactReducer from './contactReducer';
+import {
+  ADD_CONTACT, DELETE_CONTACT, SET_CURRENT, CLEAR_CURRENT, UPDATE_CONTACT, FILTER_CONTACTS, CLEAR_FILTER
+} from '../types';
+
+
+const ContactState = props => {
+  const initialState = {
+    contacts : [
+      {
+        id : 1,
+        name : "Mudassir Ali",
+        email : "ali@gmail.com",
+        phone : "+1-9800101101",
+        type : "Professional"
+      },
+      {
+        id : 2,
+        name : "Abdul Malik",
+        email : "malik@gmail.com",
+        phone : "+91-9709101101",
+        type : "Personal"
+      },
+      {
+        id : 3,
+        name : "Tom Cruise",
+        email : "tom@gmail.com",
+        phone : "+971-890098788",
+        type : "Professional"
+      }
+
+    ]
+  };
+
+  const [state, dispatch] = useReducer(contactReducer, initialState);
+
+  // Add Contact
+
+  // Delete Contact
+
+  // Set Current Contact
+  
+  //Clear Current Contact
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider
+      values = {{
+        contacts : state.contacts
+      }}>
+      { props.children }
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
